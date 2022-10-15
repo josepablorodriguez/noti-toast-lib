0427de0ffd3a1eafbafddb2ddc8688bdd1004673
josepablorodriguez <35310019+josepablorodriguez@users.noreply.github.com>
2022-10-14 22:50:05 -0600

    Update script.js
    added some nodes to the test script

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-import Toast from "./Toast.js"
+import NotiToast from "./noti-toast.js";
 
-document.querySelector("button").addEventListener("click", () => {
-  const toast = new Toast({
-    text: "Hello",
-    position: "top-right",
-    pauseOnHover: true,
-    pauseOnFocusLoss: true,
-  })
-})
+document.querySelector('button').addEventListener('click', ()=>{
+	const toast = new NotiToast({
+		text: "BYE",
+		html: `<div><u>Greet:</u></div><div>Hi, toast!</div>`,
+		//autoClose: 2000,
+		//canClose: false,
+		//position: 'top-right',
+		style: {
+			'background-color': 'yellow',
+			border: '4px solid #999',
+		},
+		//onClose: ()=>{ alert("hola"); },
+		//showProgressBar: false,
+		//pauseOnHover: false,
+		//pauseOnFocusLoss: false,
+		animation: {
+			type: 'slide',
+			duration_ms: 2000,
+		},
+		//animation: false,
+	});
+});
